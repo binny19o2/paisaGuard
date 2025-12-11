e8d528da67154f76522f1fadbd1dcf105377ffd2
binny <bhuvneshgojra1902@gmail.com>
2025-12-11 22:32:53 +0530

    initial cofigurations

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyC59-jnuqgsdX5Uact83_ExtAR7ky7-y2Y",
+        authDomain: "paisaguard-859a3.firebaseapp.com",
+        projectId: "paisaguard-859a3",
+        storageBucket: "paisaguard-859a3.firebasestorage.app",
+        messagingSenderId: "677610257332",
+        appId: "1:677610257332:web:30678d18f9a33dba6d50e1",
+        measurementId: "G-JRQ1S08Q8Q"
+    }
+};
